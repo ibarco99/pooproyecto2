cdf3b7c7933aaf68207b8d0305baed3220ed2fbe
ibarco99 <39972500+ibarco99@users.noreply.github.com>
2018-11-24 21:10:32 -0500

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <random>
+using namespace std;
+
+
+class menu {
+    int x;
+public:
+    int menuu();
+    int menusudoku();
+    void menudibujar();
+    void dibujar2();
+    void dibujar3();
+
+};
