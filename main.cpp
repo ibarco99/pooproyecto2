95f28e335c4e95741c9fa0218e7e4ff3b629d35e
ibarco99 <39972500+ibarco99@users.noreply.github.com>
2018-11-26 21:52:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,14 +6,19 @@
 #include <random>
 #include "menu.h"
 #include "sudoku9.h"
+#include "sudoku4.h"
 
 using namespace std;
 
 
 int main(){
     menu s1;
-    if (s1.menuu()==1){
+    int a=s1.menuu();
+    if (a==1){
         sudoku9 juego;
+        juego.get_numero();
+    } else if(a==2){
+        sudoku4 juego;
         juego.get_numero();
         }
 
